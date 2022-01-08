0d0b1ef39d825b83bf5b625a3ae44f1b2e06bd2c
arjunraghav <arjunraghav007@gmail.com>
2022-01-08 21:18:06 +0530

    reinit frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,22 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
-import * as serviceWorker from "./serviceWorker";
 import "./index.css";
 import App from "./App";
-
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import reportWebVitals from "./reportWebVitals";
+import AppBar from "./components/AppBar";
 
 ReactDOM.render(
-	<Router>
-		<React.StrictMode>
-			<Header />
-			<Switch>
-				<Route exact path='/' component={App} />
-			</Switch>
-			<Footer />
-		</React.StrictMode>
-	</Router>,
+	<React.StrictMode>
+		<AppBar />
+		<App />
+	</React.StrictMode>,
 	document.getElementById("root")
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
