22dc474f75f65fea918147d88d5499938a49b4e9
arjunraghav <arjunraghav007@gmail.com>
2022-01-09 12:29:42 +0530

    added header and footer component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import AppBar from "./components/AppBar";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<AppBar />
 		<App />
 	</React.StrictMode>,
 	document.getElementById("root")
